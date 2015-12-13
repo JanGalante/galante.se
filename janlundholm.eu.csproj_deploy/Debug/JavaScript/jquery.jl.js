051f65ea9de60f9b1bbd265a2a5357ebed208408
JanGalante <ave_caesar@hotmail.com>
2015-12-13 20:53:26 +0100

    Removing project janlundholm.eu.csproj_deploy

--- janlundholm.eu.csproj_deploy/Debug/JavaScript/jquery.jl.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function(){
-    //Letar efter fancyboxes
-    $(".fancybox").fancybox({
-				'frameWidth': 300, 
-				'frameHeight': 300, 
-				//'centerOnScroll': false,
-				'hideOnContentClick': false
-			});
-});
--- janlundholm.eu.csproj_deploy/Release/HttpHandlers/Wc2010.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" CodeBehind="Wc2010.ashx.cs" Class="janlundholm.eu.HttpHandlers.Wc2010" %>
